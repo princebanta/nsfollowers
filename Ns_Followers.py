8c393a58af7b7542ee34538e19f919dc50167788
princebanta <141979410+princebanta@users.noreply.github.com>
2024-05-16 15:37:55 +0530

    Ns_Followers.py

--- Ns_Followers.py
+++ Ns_Followers.py
@@ -11,9 +11,9 @@ class Ns_Followers:
         self.requests=requests
         self.__AES_NoPadding = AES_CBC_NoPadding()
 
-        self.__userid='...' #Enter UserID Here
-        self.__username='...' #Enter UserName Here
-        self._session='...' #Enter SessionID Here
+        self.__userid='66963633855' #Enter UserID Here
+        self.__username='andrew839200' #Enter UserName Here
+        self._session='66963633855%3ASNxeEucEapCmBQ%3A27%3AAYf7dhiRY6pZFSOQBuexRhvDmg1iEqPhN5-BZR0Dag' #Enter SessionID Here
 
         self.__Pk=self.__userid.encode()+b'*'+str(random.randint(111,999)).encode()
         self.__Ip=os.urandom(8).hex().encode()+b'*'+str(random.randint(111,999)).encode()
@@ -33,7 +33,7 @@ class Ns_Followers:
             'pk': self.__AES_NoPadding.encrypt(self.__Pk).hex(),
             'ip': self.__AES_NoPadding.encrypt(self.__Ip).hex(),
             'properties': self.__AES_NoPadding.encrypt(self.__Device).hex(),
-            'user': json.dumps({'api_token':'', 'pk':self.__userid, 'profile_image':'', 'fullname':self.__username, 'sessionid':self._session, 'sessionid_threads':'null', 'is_threads_enabled':'1', 'csrftoken':'', 'coins_count':'0', 'username':self.__username, 'device_id':'', 'family_device_id':'', 'android_id':'', 'user_agent':'', 'ig_did':'', 'ig_nrcb':'', 'mid':'', 'rur':f'', 'shbid':'', 'shbts':'', 'datr':'null', 'dpr':'null', 'region_hint':'null', 'diamonds_count':'0', 'is_miner':'-1', 'www_claim':'', 'phone_id':'', 'nitrogen_status':'0', 'nitrogen_coins':'0', 'last_follow_session_time':'0', 'followSessionCount':'0', 'isMarked':'false', 'isBlocked':'false', 'isBreathed':'false', 'isWaitingForPosts':'false', 'breathTime':'0', 'bio':'null', 'isWorkingAutoPlus':'false', 'status':'0', 'statusMessage':'null', 'LimitCount':'99999', 'pigeon_session_id':''}),
+            'user': json.dumps({'api_token':'', 'pk':self.__userid, 'profile_image':'', 'fullname':self.__username, 'sessionid':self._session, 'sessionid_threads':'null', 'is_threads_enabled':'1', 'csrftoken':'', 'coins_count':'1000', 'username':self.__username, 'device_id':'', 'family_device_id':'', 'android_id':'', 'user_agent':'', 'ig_did':'', 'ig_nrcb':'', 'mid':'', 'rur':f'', 'shbid':'', 'shbts':'', 'datr':'null', 'dpr':'null', 'region_hint':'null', 'diamonds_count':'0', 'is_miner':'-1', 'www_claim':'', 'phone_id':'', 'nitrogen_status':'0', 'nitrogen_coins':'0', 'last_follow_session_time':'0', 'followSessionCount':'0', 'isMarked':'false', 'isBlocked':'false', 'isBreathed':'false', 'isWaitingForPosts':'false', 'breathTime':'0', 'bio':'null', 'isWorkingAutoPlus':'false', 'status':'0', 'statusMessage':'null', 'LimitCount':'99999', 'pigeon_session_id':''}),
             'req_type': self.__AES_NoPadding.encrypt(b'login*'+str(random.randint(111,999)).encode()).hex(),
             'req_token1': self.__AES_NoPadding.encrypt(self.__AES_NoPadding.encrypt(b'tZZfV*'+str(random.randint(111,999)).encode()).hex().encode()+b'aNw90zallcQ3BSf9866xaNw90zallc'+self.__AES_NoPadding.encrypt(self.__Pk).hex().encode()+b'*'+str(random.randint(111,999)).encode()).hex(),
             'req_token2': self.__AES_NoPadding.encrypt(self.__AES_NoPadding.encrypt(self.__AES_NoPadding.encrypt(b'tZZfV*'+str(random.randint(11,99)).encode()).hex().encode()+b'aNw90zallc1l8IY40pjuaNw90zallc'+self.__AES_NoPadding.encrypt(self.__Pk).hex().encode()+b'*'+str(random.randint(111,999)).encode()).hex().encode()+b'*'+str(random.randint(111,999)).encode()).hex(),
@@ -116,10 +116,10 @@ if __name__ == '__main__':
             if "suggests" in suff.text:
                 for items in suff.json()['suggests']:
                     print(cl.PlaceOrder(items))
-                    time.sleep(random.randint(10,15))
+                    time.sleep(random.randint(0.1,0.5))
             else:
                 print(suff.text)
-                time.sleep(10)
+                time.sleep(0.1)
         except Exception as E:
             print(E)
-            time.sleep(5)
+            time.sleep(0.2)
